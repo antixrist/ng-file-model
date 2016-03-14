4a4ad0ae19e59dbf16d3a7d3141584f7203f8e8a
antixrist <ya.antixrist@gmail.com>
2016-03-14 16:30:32 +0300

    Update ng-file-model.js

--- ng-file-model.js
+++ ng-file-model.js
@@ -1,46 +1,56 @@
 (function () {
-    'use strict';
-    angular.module('ng-file-model', [])
-    .directive('ngFileModel', [function () {
-        return {
-            scope: {
-                ngFileModel: "="
-            },
-            link: function (scope, element, attributes) {
-                element.bind('change', function (changeEvent) {
-                    var files           = [],
-                        filesReady      = 0,
-                        rawFiles        = changeEvent.target.files,
-                        rawFilesCount   = rawFiles.length,
-                        onFilesReady    = function () {
-                            $scope.$apply(function () {
-                                scope.ngFileModel = (attributes.multiple) ? files : files[0];
-                            });
-                        };
+  'use strict';
 
-                    rawFiles.forEach(function (file) {
-                        var reader = new FileReader();
-                        reader
-                            .readAsDataURL(file)
-                            .onload = (function (file) {
-                                return function (loadEvent) {
-                                    files.push({
-                                        lastModified: file.lastModified,
-                                        lastModifiedDate: file.lastModifiedDate,
-                                        name: file.name,
-                                        size: file.size,
-                                        type: file.type,
-                                        data: file.result
-                                    });
-                            
-                                    if (++filesReady == rawFilesCount) {
-                                        onFilesReady();
-                                    }
-                                };
-                            })(file);
-                    });
+  angular
+    .module('ng-file-model', [])
+    .directive('ngFileModel', ngFileModelFactory);
+
+  function ngFileModelFactory () {
+    return {
+      scope: {
+        ngFileModel: '=',
+        ngMultiple:  '='
+      },
+      link:  function (scope, element, attributes) {
+        scope.$watch('ngMultiple', function (newValue) {
+          element.get(0).multiple = !!newValue;
+        });
+
+        element.bind('change', function (changeEvent) {
+          var files         = [],
+              filesReady    = 0,
+              rawFiles      = changeEvent.target.files,
+              rawFilesCount = rawFiles.length,
+              onFilesReady  = function () {
+                scope.$apply(function () {
+                  scope.ngFileModel = (attributes.multiple) ? files : files[0];
                 });
-            }
-        }
-    }]);
+              };
+
+          [].forEach.call(rawFiles, function (file) {
+            var reader    = new FileReader();
+            reader.onload = (function (file) {
+              return function (loadEvent) {
+                if (loadEvent.type == 'load') {
+                  files.push({
+                    lastModified:     file.lastModified,
+                    lastModifiedDate: file.lastModifiedDate,
+                    name:             file.name,
+                    size:             file.size,
+                    type:             file.type,
+                    data:             loadEvent.currentTarget.result
+                  });
+                }
+
+                if (++filesReady == rawFilesCount) {
+                  onFilesReady();
+                }
+              };
+            })(file);
+            reader.readAsDataURL(file);
+          });
+        });
+      }
+    }
+  }
 })();
