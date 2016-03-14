707f587d4302cdae2fe19c34c3cafac97ece0d6b
antixrist <ya.antixrist@gmail.com>
2016-03-14 16:00:19 +0300

    Update ng-file-model.js

--- ng-file-model.js
+++ ng-file-model.js
@@ -1,27 +1,44 @@
 (function () {
     'use strict';
     angular.module('ng-file-model', [])
-    .directive("ngFileModel", [function () {
+    .directive('ngFileModel', [function () {
         return {
             scope: {
                 ngFileModel: "="
             },
             link: function (scope, element, attributes) {
-                element.bind("change", function (changeEvent) {
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
-                        scope.$apply(function () {
-                            scope.ngFileModel = {
-                                lastModified: changeEvent.target.files[0].lastModified,
-                                lastModifiedDate: changeEvent.target.files[0].lastModifiedDate,
-                                name: changeEvent.target.files[0].name,
-                                size: changeEvent.target.files[0].size,
-                                type: changeEvent.target.files[0].type,
-                                data: loadEvent.target.result
-                            };
-                        });
-                    }
-                    reader.readAsDataURL(changeEvent.target.files[0]);
+                element.bind('change', function (changeEvent) {
+                    var files           = [],
+                        filesReady      = 0,
+                        rawFiles        = changeEvent.target.files,
+                        rawFilesCount   = rawFiles.length,
+                        onFilesReady    = function () {
+                            $scope.$apply(function () {
+                                scope.ngFileModel = (attributes.multiple) ? files : files[0];
+                            });
+                        };
+
+                    rawFiles.forEach(function (file) {
+                        var reader = new FileReader();
+                        reader
+                            .readAsDataURL(file)
+                            .onload = (function (file) {
+                                return function (loadEvent) {
+                                    files.push({
+                                        lastModified: file.lastModified,
+                                        lastModifiedDate: file.lastModifiedDate,
+                                        name: file.name,
+                                        size: file.size,
+                                        type: file.type,
+                                        data: file.result
+                                    });
+                            
+                                    if (++filesReady == rawFilesCount) {
+                                        onFilesReady();
+                                    }
+                                };
+                            })(file);
+                    });
                 });
             }
         }
